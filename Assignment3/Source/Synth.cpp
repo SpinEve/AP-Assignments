a0c21ff0436d14140506005818698d915d370f74
Spin Eve <20128868+SpinEve@users.noreply.github.com>
2021-04-26 14:27:07 +0800

    Add oscillator and divide Synth.h

---
+++ Assignment3/Source/Synth.cpp
@@ -0,0 +1,37 @@
+#pragma once
+#include "Synth.h"
+
+#include <JuceHeader.h>
+
+SynthVoice::SynthVoice() { sampleOsc = new SinOsc(); }
+void SynthVoice::startNote(int midiNoteNumber, float velocity,
+                           juce::SynthesiserSound* sound,
+                           int currentPitchWheelPosition) {
+  float freq = juce::MidiMessage::getMidiNoteInHertz(midiNoteNumber);
+  sampleOsc->setFreq(freq);
+  playing = true;
+}
+void SynthVoice::stopNote(float velocity, bool allowTailOff) {
+  clearCurrentNote();
+  playing = false;
+}
+void SynthVoice::renderNextBlock(juce::AudioBuffer<double>& outputBuffer,
+                                 int startSample, int numSamples) {
+  if (playing) {
+    for (int sampleIndex = startSample;
+         sampleIndex < (startSample + numSamples); sampleIndex++) {
+      // Oscillator
+      float currentSample = sampleOsc->loop();
+      // Noise Generator
+      // float currentSample = random.nextFloat() * 2 - 1.0;
+      for (int chan = 0; chan < outputBuffer.getNumChannels(); chan++) {
+        outputBuffer.addSample(chan, sampleIndex,
+                               currentSample * 0.2 * (volume / 100.f));
+      }
+    }
+  }
+}
+bool SynthVoice::canPlaySound(juce::SynthesiserSound* sound) {
+  return dynamic_cast<SynthSound*>(sound) != nullptr;
+}
+void SynthVoice::setVolume(float _volume) { volume = _volume; }
--- Assignment3/Source/Synth.h
+++ Assignment3/Source/Synth.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <JuceHeader.h>
 
+#include "Oscillator.h"
+
 class SynthSound : public juce::SynthesiserSound {
  public:
   bool appliesToNote(int) override { return true; }
@@ -9,41 +11,21 @@ class SynthSound : public juce::SynthesiserSound {
 
 class SynthVoice : public juce::SynthesiserVoice {
  public:
-  SynthVoice() {}
+  SynthVoice();
   void startNote(int midiNoteNumber, float velocity,
                  juce::SynthesiserSound* sound,
-                 int currentPitchWheelPosition) override {
-    float freq = juce::MidiMessage::getMidiNoteInHertz(midiNoteNumber);
-    playing = true;
-  }
-  void stopNote(float velocity, bool allowTailOff) override {
-    clearCurrentNote();
-    playing = false;
-  }
+                 int currentPitchWheelPosition) override;
+  void stopNote(float velocity, bool allowTailOff) override;
   void renderNextBlock(juce::AudioBuffer<double>& outputBuffer, int startSample,
-                       int numSamples) override {
-    if (playing) {
-      for (int sampleIndex = startSample;
-           sampleIndex < (startSample + numSamples); sampleIndex++) {
-        // TODO: Oscillator
-        // Noise Generator
-        float currentSample = random.nextFloat() * 2 - 1.0;
-        for (int chan = 0; chan < outputBuffer.getNumChannels(); chan++) {
-          outputBuffer.addSample(chan, sampleIndex,
-                                 currentSample * 0.2 * (volume / 100.f));
-        }
-      }
-    }
-  }
-  void pitchWheelMoved(int) override {}
-  void controllerMoved(int, int) override {}
-  bool canPlaySound(juce::SynthesiserSound* sound) override {
-    return dynamic_cast<SynthSound*>(sound) != nullptr;
-  }
-  void setVolume(float _volume) { volume = _volume; }
+                       int numSamples) override;
+  void pitchWheelMoved(int) override;
+  void controllerMoved(int, int) override;
+  bool canPlaySound(juce::SynthesiserSound* sound) override;
+  void setVolume(float _volume);
 
  private:
   bool playing = false;
   float volume = 50.f, freq = 440.f;
   juce::Random random;
+  SinOsc* sampleOsc;
 };
